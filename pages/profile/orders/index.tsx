d8809688b5cae66e24f6dcff85bade00f64c2b1e
CaseyMartin23 <cm232311@gmail.com>
2022-09-09 12:12:47 +0200

    Added OrderItems

--- pages/profile/orders/index.tsx
+++ pages/profile/orders/index.tsx
@@ -1,28 +1,102 @@
-import React from "react";
+import React, { useState } from "react";
 import { NextPage } from "next";
 import PageLayout from "../../../comps/pageLayout";
 import Head from "next/head";
 import BackwardsNavbar from "../../../comps/backwardsNavbar";
+import { useRouter } from "next/router";
 
-const OrderItem: React.FC = () => {
+type OrderItemType = {
+  id: string;
+  status: string;
+  images: string[];
+  deliveredDate: string;
+};
+
+type OrderItemProps = {
+  orderItem: OrderItemType;
+  handleClick: (id: string) => void;
+};
+
+const OrderItemImage: React.FC<{ src: string; alt: string }> = ({
+  src,
+  alt,
+}) => {
+  return <div className="order-item-image rounded bg-cyan-400"></div>;
+};
+
+const OrderItem: React.FC<OrderItemProps> = ({ orderItem, handleClick }) => {
+  const { id, status, deliveredDate, images } = orderItem;
+
   return (
-    <div className="bg-white mb-1 px-3 py-2 rounded">
-      <div className="my-1">Status and Date</div>
-      <div className="my-1">Signed by Name</div>
-      <div className="flex flex-row my-1 max-w-full overflow-auto bg-green-300">
-        <div className="block w-28 h-16 mx-2 rounded bg-cyan-400"></div>
-        <div className="w-28 h-16 mx-2 rounded bg-cyan-400"></div>
-        <div className="w-28 h-16 mx-2 rounded bg-cyan-400"></div>
-        <div className="w-28 h-16 mx-2 rounded bg-cyan-400"></div>
-        <div className="w-28 h-16 mx-2 rounded bg-cyan-400"></div>
-        <div className="w-28 h-16 mx-2 rounded bg-cyan-400"></div>
-        <div className="w-28 h-16 mx-2 rounded bg-cyan-400"></div>
+    <div
+      onClick={() => handleClick(id)}
+      className="bg-white mb-1 px-3 py-2 rounded"
+    >
+      <div className="my-1">
+        {status} {deliveredDate}
+      </div>
+      <div className="order-item-image-container">
+        {images.map((url, idx) => (
+          <OrderItemImage key={`${url}-${idx}`} src={url} alt={url} />
+        ))}
       </div>
     </div>
   );
 };
 
 const Orders: NextPage = (props) => {
+  const { push } = useRouter();
+  const [allOrders, setAllOrders] = useState<OrderItemType[]>([
+    {
+      id: "TEST-ORDER-ID-01",
+      status: "TEST-ORDER-STATUS-01",
+      images: [
+        "TEST-ORDER-IMAGE-01",
+        "TEST-ORDER-IMAGE-02",
+        "TEST-ORDER-IMAGE-03",
+        "TEST-ORDER-IMAGE-04",
+      ],
+      deliveredDate: "TEST-ORDER-DELIVERED-DATE-01",
+    },
+    {
+      id: "TEST-ORDER-ID-02",
+      status: "TEST-ORDER-STATUS-02",
+      images: [
+        "TEST-ORDER-IMAGE-01",
+        "TEST-ORDER-IMAGE-02",
+        "TEST-ORDER-IMAGE-03",
+        "TEST-ORDER-IMAGE-04",
+      ],
+      deliveredDate: "TEST-ORDER-DELIVERED-DATE-02",
+    },
+    {
+      id: "TEST-ORDER-ID-03",
+      status: "TEST-ORDER-STATUS-03",
+      images: [
+        "TEST-ORDER-IMAGE-01",
+        "TEST-ORDER-IMAGE-02",
+        "TEST-ORDER-IMAGE-03",
+        "TEST-ORDER-IMAGE-04",
+      ],
+      deliveredDate: "TEST-ORDER-DELIVERED-DATE-03",
+    },
+    {
+      id: "TEST-ORDER-ID-04",
+      status: "TEST-ORDER-STATUS-04",
+      images: [
+        "TEST-ORDER-IMAGE-01",
+        "TEST-ORDER-IMAGE-02",
+        "TEST-ORDER-IMAGE-03",
+        "TEST-ORDER-IMAGE-04",
+      ],
+      deliveredDate: "TEST-ORDER-DELIVERED-DATE-04",
+    },
+  ]);
+
+  const onOrderItemClick = (id: string) => {
+    push(`/profile/orders/${id}`);
+  };
+
   return (
     <PageLayout>
       <Head>
@@ -40,13 +114,13 @@ const Orders: NextPage = (props) => {
           <div className="bg-white border">filter</div>
 
           <div className="p-1">
-            <OrderItem />
-            <OrderItem />
-            <OrderItem />
-            <OrderItem />
-            <OrderItem />
-            <OrderItem />
-            <OrderItem />
+            {allOrders.map((order, index) => (
+              <OrderItem
+                key={`${order.id}-${index}`}
+                orderItem={order}
+                handleClick={onOrderItemClick}
+              />
+            ))}
           </div>
         </div>
       </main>
