e53579a813834ec637722a0fc66dff61cf0805e5
CaseyMartin23 <cm232311@gmail.com>
2022-09-17 09:11:19 +0200

    updated register form

--- pages/register.tsx
+++ pages/register.tsx
@@ -1,11 +1,73 @@
-import React from "react";
+import React, { useState } from "react";
 import { NextPage } from "next";
 import Head from "next/head";
 import Link from "next/link";
 
 import PageLayout from "../comps/pageLayout";
 
+type formDataType = {
+  firstname: string;
+  lastname: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+};
+
+type formErrorsType = {
+  firstname: string;
+  lastname: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+};
+
 const RegisterPage: NextPage = () => {
+  const [formData, setFormData] = useState<formDataType>({
+    firstname: "",
+    lastname: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+  const [formErrors, setFormErrors] = useState<formErrorsType>({
+    firstname: "",
+    lastname: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const validateFormValue = (field: string, value: string) => {
+    switch (field) {
+      case "firstname":
+        break;
+
+      case "lastname":
+        break;
+
+      case "email":
+        break;
+
+      case "password":
+        break;
+
+      case "confirmPassword":
+        break;
+    }
+  };
+
+  const onInputChange = ({ target }: React.ChangeEvent<HTMLInputElement>) => {
+    const { value, dataset } = target;
+    const { field } = dataset;
+    validateFormValue(field ? field : "", value);
+    setFormData({ ...formData, [`${field}`]: value });
+  };
+
+  const onSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    console.log("formData:", formData);
+  };
+
   return (
     <PageLayout>
       <Head>
@@ -25,16 +87,17 @@ const RegisterPage: NextPage = () => {
             </h4>
           </div>
           <div>
-            <form onSubmit={() => {}}>
+            <form onSubmit={onSubmit}>
               <div className="flex flex-col my-3">
                 <input
                   className="p-2 mt-1 outline-none bg-slate-100 border-b-accent-color focus:border-b-2"
                   type="text"
                   placeholder="First name"
                   data-field="firstname"
-                  value={""}
-                  onChange={() => {}}
+                  value={formData.firstname}
+                  onChange={onInputChange}
                 />
+                <span>{formErrors.firstname}</span>
               </div>
 
               <div className="flex flex-col my-3">
@@ -43,9 +106,10 @@ const RegisterPage: NextPage = () => {
                   type="text"
                   placeholder="Last name"
                   data-field="lastname"
-                  value={""}
-                  onChange={() => {}}
+                  value={formData.lastname}
+                  onChange={onInputChange}
                 />
+                <span>{formErrors.lastname}</span>
               </div>
 
               <div className="flex flex-col my-3">
@@ -54,9 +118,10 @@ const RegisterPage: NextPage = () => {
                   type="email"
                   placeholder="Email"
                   data-field="email"
-                  value={""}
-                  onChange={() => {}}
+                  value={formData.email}
+                  onChange={onInputChange}
                 />
+                <span>{formErrors.email}</span>
               </div>
 
               <div className="flex flex-col my-3">
@@ -65,9 +130,10 @@ const RegisterPage: NextPage = () => {
                   type="password"
                   placeholder="Password"
                   data-field="password"
-                  value={""}
+                  value={formData.password}
                   onChange={() => {}}
                 />
+                <span>{formErrors.password}</span>
               </div>
 
               <div className="flex flex-col my-3">
@@ -79,6 +145,7 @@ const RegisterPage: NextPage = () => {
                   value={""}
                   onChange={() => {}}
                 />
+                <span>{formErrors.firstname}</span>
               </div>
 
               <button
